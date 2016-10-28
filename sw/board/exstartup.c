ae238b5e79c4aca21566f82d6b0d6b8b8113c5b2
Me <me@example.com>
2016-10-28 07:36:17 -0400

    Added the license notification

--- sw/board/exstartup.c
+++ sw/board/exstartup.c
@@ -1,3 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename: 	exstartup.c
+//
+// Project:	OpenArty, an entirely open SoC based upon the Arty platform
+//
+// Purpose:	A fun example program that runs on the Arty, just to show
+//		that the minimum set of peripherals (LEDs, color LEDs, buttons,
+//	switches, etc.) work.
+//
+// Creator:	Dan Gisselquist, Ph.D.
+//		Gisselquist Technology, LLC
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2015-2016, Gisselquist Technology, LLC
+//
+// This program is free software (firmware): you can redistribute it and/or
+// modify it under the terms of  the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  (It's in the $(ROOT)/doc directory, run make with no
+// target there if the PDF file isn't present.)  If not, see
+// <http://www.gnu.org/licenses/> for a copy.
+//
+// License:	GPL, v3, as defined and found on www.gnu.org,
+//		http://www.gnu.org/licenses/gpl.html
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//
 #include "artyboard.h"
 #include "zipsys.h"
 
