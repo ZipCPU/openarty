3b80c41c5aea211029d872ad8f661fa9b9e83ee4
ZipCPU <dgisselq@ieee.org>
2017-02-10 09:38:49 -0500

    New capability to load ELF files directly from command line

---
+++ sim/verilated/zipelf.h
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Filename:	zipelf.h
+//
+// Project:	OpenArty, an entirely open SoC based upon the Arty platform
+//
+// Purpose:	
+//
+//
+// Creator:	Dan Gisselquist, Ph.D.
+//		Gisselquist Technology, LLC
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2015-2016, Gisselquist Technology, LLC
+//
+// This program is free software (firmware): you can redistribute it and/or
+// modify it under the terms of  the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  (It's in the $(ROOT)/doc directory, run make with no
+// target there if the PDF file isn't present.)  If not, see
+// <http://www.gnu.org/licenses/> for a copy.
+//
+// License:	GPL, v3, as defined and found on www.gnu.org,
+//		http://www.gnu.org/licenses/gpl.html
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+#ifndef	ZIPELF_H
+#define	ZIPELF_H
+
+#include <stdint.h>
+
+class	ELFSECTION {
+public:
+	uint32_t	m_start, m_len;
+	char		m_data[4];
+};
+
+bool	iself(const char *fname);
+void	elfread(const char *fname, uint32_t &entry, ELFSECTION **&sections);
+
+#endif
