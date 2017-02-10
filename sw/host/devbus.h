62475fd11bca7813791ba96e59260560b260ee7a
ZipCPU <dgisselq@ieee.org>
2017-02-10 10:12:34 -0500

    Added documentation describing devbus interface

--- sw/host/devbus.h
+++ sw/host/devbus.h
@@ -1,13 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Filename: 	devbus.h
+// Filename:	devbus.h
 //
 // Project:	OpenArty, an entirely open SoC based upon the Arty platform
 //
 // Purpose:	The purpose of this file is to document an interface which
-//		any devic with a bus, whether it be implemented over a UART,
-//	an ethernet, or a PCI express bus, must implement.  This describes only
-//	an interface, and not how that interface is to be accomplished.
+//		any device with a bus, whether it be implemented over a UART,
+//	an ethernet, or a PCI express bus, must implement.  This describes
+//	only an interface, and not how that interface is to be accomplished.
+//
+//	The neat part of this interface is that, if programs are designed to
+//	work with it, than the implementation details may be changed later
+//	and any program that once worked with the interface should be able
+//	to continue to do so.  (i.e., switch from a UART controlled bus to a
+//	PCI express controlled bus, with minimal change to the software of
+//	interest.)
 //
 //
 // Creator:	Dan Gisselquist, Ph.D.
@@ -28,7 +35,7 @@
 // for more details.
 //
 // You should have received a copy of the GNU General Public License along
-// with this program.  (It's in the $(ROOT)/doc directory, run make with no
+// with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
 // target there if the PDF file isn't present.)  If not, see
 // <http://www.gnu.org/licenses/> for a copy.
 //
@@ -61,18 +68,56 @@ public:
 	virtual	void	close(void) = 0;
 
 	// Write a single value to a single address
+	//	a is the address of the value to be read as it exists on the
+	//		wishbone bus within the FPGA.
+	//	v is the singular value to write to this address
 	virtual	void	writeio(const BUSW a, const BUSW v) = 0;
 
 	// Read a single value to a single address
+	//	a is the address of the value to be read as it exists on the
+	//		wishbone bus within the FPGA.
+	//	This function returns the value read from the device wishbone
+	//		at address a.
 	virtual	BUSW	readio(const BUSW a) = 0;
 
 	// Read a series of values from values from a block of memory
+	//	a is the address of the value to be read as it exists on the
+	//		wishbone bus within the FPGA.
+	//	len is the number of words to read
+	//	buf is a pointer to a place to store the words once read.
+	// This is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		buf[i] = readio(a+i);
+	// only it's faster in our implementation.
 	virtual	void	readi(const BUSW a, const int len, BUSW *buf) = 0;
 
-	// Read a series of values from the same address in memory
+	// Read a series of values from the same address in memory.  This 
+	// call is identical to readi, save that the address is not incremented
+	// from one read to the next.  It is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		buf[i] = readio(a);
+	// only it's faster in our implementation.
+	//
 	virtual	void	readz(const BUSW a, const int len, BUSW *buf) = 0;
 
+	// Write a series of values into a block of memory on the FPGA
+	//	a is the address of the value to be written as it exists on the
+	//		wishbone bus within the FPGA.
+	//	len is the number of words to write
+	//	buf is a pointer to a place to from whence to grab the data
+	//		to be written.
+	// This is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		writeio(a+i, buf[i]);
+	// only it's faster in our implementation.
 	virtual	void	writei(const BUSW a, const int len, const BUSW *buf) = 0;
+	// Write a series of values into the same address on the FPGA bus.  This
+	// call is identical to writei, save that the address is not incremented
+	// from one write to the next.  It is equivalent to:
+	//	for(int i=0; i<len; i++)
+	//		writeio(a, buf[i]);
+	// only it's faster in our implementation.
+	//
 	virtual	void	writez(const BUSW a, const int len, const BUSW *buf) = 0;
 
 	// Query whether or not an interrupt has taken place
